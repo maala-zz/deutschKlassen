589dbd6649ba2ff2edcc75bce47aa544189db765
Ext_Maala.Mhrez <maala.mhrez.it@hotmail.com>
2020-04-30 06:29:06 +0300

    init project

---
+++ src/deutschklassen/WarumNichtXmlReader.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deutschklassen;
+
+/**
+ *
+ * @author ASUS
+ */
+public class WarumNichtXmlReader {
+
+    //<editor-fold defaultstate="collapsed" desc="Proprties">
+    private final String filePath;
+
+//</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="Constructors">
+    public WarumNichtXmlReader(String filePath) {
+        this.filePath = filePath;
+    }
+//</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Methods">
+    public String gtFilePath() {
+        return this.filePath;
+    }
+    
+
+    
+//</editor-fold>
+
+}
